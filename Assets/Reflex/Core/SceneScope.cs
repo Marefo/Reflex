60cc1aaef13075d4d2fb04225f73a2841061c025
Marefo <79011582+Marefo@users.noreply.github.com>
2025-07-16 17:58:23 +0300

    Removed OnSceneLoaded  call from SceneScope

--- Assets/Reflex/Core/SceneScope.cs
+++ Assets/Reflex/Core/SceneScope.cs
@@ -1,18 +1,11 @@
-using Reflex.Injectors;
 using Reflex.Logging;
 using UnityEngine;
 using UnityEngine.Pool;
 
 namespace Reflex.Core
 {
-    [DefaultExecutionOrder(int.MinValue)]
     public sealed class SceneScope : MonoBehaviour
     {
-        private void Awake()
-        {
-            UnityInjector.OnSceneLoaded.Invoke(gameObject.scene, this);
-        }
-
         public void InstallBindings(ContainerBuilder containerBuilder)
         {
             using var pooledObject = ListPool<IInstaller>.Get(out var installers);
